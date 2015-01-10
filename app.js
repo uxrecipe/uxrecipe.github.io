7406b628ca26e2f16dd3ded88180c7f24314aa74
uxrecipe <alecsandru_grigoriu@yahoo.com>
2015-01-10 18:28:23 +0200

    added toggle feature on inputs

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
     $(":checkbox").labelauty();
 });
+
+
+
+$(document).ready(function(){
+    $('input[type="checkbox"]').click(function(){
+        if($(this).attr("value")=="Expert"){
+            $(".result").toggle();
+        }
+    });
+});
