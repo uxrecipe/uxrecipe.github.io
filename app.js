71c997da838efcdbc8a27732dee407d60a2709aa
uxrecipe <alecsandru_grigoriu@yahoo.com>
2015-01-11 15:54:33 +0200

    added all the results & toggle fade

--- app.js
+++ app.js
@@ -9,10 +9,76 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('input[type="checkbox"]').click(function(){
         if($(this).attr("value")=="Expert"){
-            $(".result").toggle();
+            $(".result").fadeToggle( 300, "linear" );
         }
-        if($(this).attr("value")=="Expert2"){
-            $(".result2").toggle();
+        if($(this).attr("value")=="Client"){
+            $(".result2").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="Competitor"){
+            $(".result3").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="Requirements"){
+            $(".result4").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="SEO"){
+            $(".result5").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="Mobile"){
+            $(".result6").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="Contextual"){
+            $(".result7").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="Surveys"){
+            $(".result8").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="Personas"){
+            $(".result9").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="User"){
+            $(".result10").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="Storyboards"){
+            $(".result11").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="Journey"){
+            $(".result12").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="Sitemap"){
+            $(".result13").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="Card Sorting"){
+            $(".result14").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="IA"){
+            $(".result15").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="Sketching"){
+            $(".result16").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="Wireframes"){
+            $(".result17").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="Styleguide"){
+            $(".result18").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="Mockups"){
+            $(".result19").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="Prototyping"){
+            $(".result20").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="AB"){
+            $(".result21").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="Guerilla"){
+            $(".result22").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="Lab"){
+            $(".result23").fadeToggle( 300, "linear" );
+        }
+        if($(this).attr("value")=="Remote"){
+            $(".result24").fadeToggle( 300, "linear" );
         }
     });
 });
