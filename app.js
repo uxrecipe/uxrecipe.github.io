7b9b5c0a547300fe5f74ef100152fb3b4ffc405d
uxrecipe <alecsandru_grigoriu@yahoo.com>
2016-03-12 23:57:37 +0200

    popup for ios version

--- app.js
+++ app.js
@@ -19,6 +19,34 @@ $(document).ready(function(){
         }, 500);
         return false;
      });
+
+
+    swal({
+      title: 'UX Recipe app is now available on the Apple Store!',
+      text: 'We have been working hard to bring the app to life and that time has arrived: the iOS version is live. It has already been featured on Product Hunt and it passed over 1700 downloads.',
+      imageUrl: 'img/iosapp.jpg',
+      animation: false,
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Stay on site',
+      confirmButtonText: 'Download iOS app (Free)',
+      closeOnConfirm: false
+    },
+    function(){
+      window.location.href = 'https://itunes.apple.com/us/app/ux-recipe/id1078077486?ls=1&mt=8';
+    })
+
+//     $(document).ready(function() {
+// swal({ 
+//   title: "Error",
+//    text: "wrong user or password",
+//     type: "error" 
+//   },
+//   function(){
+//     window.location.href = 'login.html';
+// });
+
 });
 
 //html2canvas
